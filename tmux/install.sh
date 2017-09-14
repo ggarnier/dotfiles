3f70801b4ffa47e266d6155158208ae6e0faf84f
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-09-14 13:58:48 -0300

    tmux: add install script

---
+++ tmux/install.sh
@@ -0,0 +1,10 @@
+# Install tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install powerline fonts
+tmpdir=`mktemp -d`
+git clone https://github.com/powerline/fonts.git --depth=1 $tmpdir
+cd $tmpdir
+./install.sh
+cd ..
+rm -rf $tmpdir
